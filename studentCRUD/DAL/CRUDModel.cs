399bf016fa8e7b6110c07e0715f907d5c83befe7
Deepalisapte <deepalisapte1@gmail.com>
2019-06-27 01:38:22 +0530

    first commit

---
+++ studentCRUD/DAL/CRUDModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace DAL
+{
+    public class CRUDModel
+    {
+        public DataTable GetAllStudents()
+        {
+            DataTable dt = new DataTable();
+            string strConString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\DotNet\studentCRUD\student\App_Data\Database.mdf;Integrated Security=True";
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from student", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+
+        public DataTable GetStudentByID(int intStudentID)
+        {
+            DataTable dt = new DataTable();
+
+            string strConString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\DotNet\studentCRUD\student\App_Data\Database.mdf;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from student where Id=" + intStudentID, con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+
+
+        public int UpdateStudent(int intStudentID, string strStudentName, string strGender, int intAge)
+        {
+            string strConString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\DotNet\studentCRUD\student\App_Data\Database.mdf;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                string query = "Update student SET Name=@studname, age=@studage , gender=@gender where Id=@studid";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@studname", strStudentName);
+                cmd.Parameters.AddWithValue("@studage", intAge);
+                cmd.Parameters.AddWithValue("@gender", strGender);
+                cmd.Parameters.AddWithValue("@studid", intStudentID);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int InsertStudent(string strStudentName, string strGender, int intAge)
+        {
+            string strConString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\DotNet\studentCRUD\student\App_Data\Database.mdf;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                string query = "Insert into student (Name,age,gender) values(@studname, @studage , @gender)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                
+                cmd.Parameters.AddWithValue("@studname", strStudentName);
+                cmd.Parameters.AddWithValue("@studage", intAge);
+                cmd.Parameters.AddWithValue("@gender", strGender);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int DeleteStudent(int intStudentID)
+        {
+            string strConString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\DotNet\studentCRUD\student\App_Data\Database.mdf;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                string query = "Delete from student where Id=@studid";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@studid", intStudentID);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}  
+
+  
---
+++ studentCRUD/student/Controllers/studentController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+using System.Data;
+namespace student.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        public ActionResult Index()
+        {
+            CRUDModel model = new CRUDModel();
+            DataTable dt = model.GetAllStudents();
+            return View("Home", dt);
+        }
+
+        public ActionResult Insert()
+        {
+            return View("Create");
+        }
+
+        public ActionResult InsertRecord(FormCollection frm, string action)
+        {
+            if (action == "Submit")
+            {
+                CRUDModel model = new CRUDModel();
+                string name = frm["txtName"];
+                int age = Convert.ToInt32(frm["txtAge"]);
+                string gender = frm["gender"];
+                int status = model.InsertStudent(name, gender, age);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(int StudentID)
+        {
+            CRUDModel model = new CRUDModel();
+            DataTable dt = model.GetStudentByID(StudentID);
+            return View("Edit", dt);
+        }
+
+        public ActionResult UpdateRecord(FormCollection frm, string action)
+        {
+            if (action == "Submit")
+            {
+                CRUDModel model = new CRUDModel();
+                string name = frm["txtName"];
+                int age = Convert.ToInt32(frm["txtAge"]);
+                string gender = frm["gender"];
+                int id = Convert.ToInt32(frm["hdnID"]);
+                int status = model.UpdateStudent(id, name, gender, age);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Delete(int StudentID)
+        {
+            CRUDModel model = new CRUDModel();
+            model.DeleteStudent(StudentID);
+            return RedirectToAction("Index");
+        }
+    }
+}  
+
+
+    
